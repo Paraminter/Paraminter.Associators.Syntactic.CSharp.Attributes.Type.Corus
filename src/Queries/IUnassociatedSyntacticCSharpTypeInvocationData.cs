0c0c97a1072d4c46030faa02096deae56bae8328
ErikWe <erikwestberg7@gmail.com>
2024-07-16 21:10:07 +0200

    Initial implementation

---
+++ src/Queries/IUnassociatedSyntacticCSharpTypeInvocationData.cs
@@ -0,0 +1,16 @@
+﻿namespace Paraminter.Associators.Syntactic.CSharp.Type.Corus.Queries;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using System.Collections.Generic;
+
+/// <summary>Represents unassociated syntactic data about C# type arguments and C# type parameters.</summary>
+public interface IUnassociatedSyntacticCSharpTypeInvocationData
+{
+    /// <summary>The type parameters of the invocation.</summary>
+    public abstract IReadOnlyList<ITypeParameterSymbol> Parameters { get; }
+
+    /// <summary>The syntactic descriptions of the type arguments of the invocation.</summary>
+    public abstract IReadOnlyList<TypeSyntax> SyntacticArguments { get; }
+}
---
+++ tests/unit/Services/SyntacticCSharpTypeInvocationDataAssociator/FixtureFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace Paraminter.Associators.Syntactic.CSharp.Type.Corus;
+
+using Paraminter.Associators.Queries;
+using Paraminter.Associators.Syntactic.CSharp.Type.Corus.Queries;
+using Paraminter.Associators.Syntactic.CSharp.Type.Queries.Collectors;
+using Paraminter.Queries.Handlers;
+
+internal static class FixtureFactory
+{
+    public static IFixture Create()
+    {
+        SyntacticCSharpTypeInvocationDataAssociator sut = new();
+
+        return new Fixture(sut);
+    }
+
+    private sealed class Fixture
+        : IFixture
+    {
+        private readonly IQueryHandler<IGetAssociatedInvocationDataQuery<IUnassociatedSyntacticCSharpTypeInvocationData>, ISyntacticCSharpTypeInvocationDataAssociatorQueryResponseCollector> Sut;
+
+        public Fixture(
+            IQueryHandler<IGetAssociatedInvocationDataQuery<IUnassociatedSyntacticCSharpTypeInvocationData>, ISyntacticCSharpTypeInvocationDataAssociatorQueryResponseCollector> sut)
+        {
+            Sut = sut;
+        }
+
+        IQueryHandler<IGetAssociatedInvocationDataQuery<IUnassociatedSyntacticCSharpTypeInvocationData>, ISyntacticCSharpTypeInvocationDataAssociatorQueryResponseCollector> IFixture.Sut => Sut;
+    }
+}
