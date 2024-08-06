2bc7dde0696ff1810b4b68e63454726878a47609
ErikWe <erikwestberg7@gmail.com>
2024-08-06 15:08:23 +0200

    Upt dependencies to use marker-interfaces

--- src/Models/IAssociateSyntacticCSharpTypeData.cs
+++ src/Models/IAssociateSyntacticCSharpTypeData.cs
@@ -3,10 +3,13 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
+using Paraminter.Associators.Models;
+
 using System.Collections.Generic;
 
 /// <summary>Represents data used to associate syntactic C# type arguments.</summary>
 public interface IAssociateSyntacticCSharpTypeData
+    : IAssociateArgumentsData
 {
     /// <summary>The type parameters.</summary>
     public abstract IReadOnlyList<ITypeParameterSymbol> Parameters { get; }
