f1c7a5b9c70dc03fc8a19736e3494696d03404a2
ErikWe <erikwestberg7@gmail.com>
2024-12-31 18:44:56 +0100

    Update dependencies

--- src/Core/Errors/Commands/IHandleDifferentNumberOfArgumentsAndParametersCommand.cs
+++ src/Core/Errors/Commands/IHandleDifferentNumberOfArgumentsAndParametersCommand.cs
@@ -1,7 +1,5 @@
 ﻿namespace Paraminter.Associating.CSharp.Type.Corus.Errors.Commands;
 
-using Paraminter.Cqs;
-
 /// <summary>Represents a command to handle an error encountered when associating syntactic C# type arguments with parameters, caused by there being a different number of arguments and parameters.</summary>
 public interface IHandleDifferentNumberOfArgumentsAndParametersCommand
     : ICommand
